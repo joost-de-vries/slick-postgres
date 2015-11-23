50927328d495f8d9e2c7b00c5fc7dd9ffc432317
Joost de Vries <joost@ziener.org>
2015-11-23 10:54:32 +0100

    examples

--- src/main/scala/ExampleQueries.scala
+++ src/main/scala/ExampleQueries.scala
@@ -1,30 +1,45 @@
 import MyPostgresDriver.api._
-import slick.backend.DatabasePublisher
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration.Duration
 import scala.concurrent.{Await, Future}
+
 //import slick.driver.H2Driver.api._
 
 object ExampleQueries extends App {
   val db = Database.forConfig("postgres")
 
-    // The query interface for the Suppliers table
-    val suppliers: TableQuery[Suppliers] = TableQuery[Suppliers]
+  // The query interface for the Suppliers table
+  val suppliers: TableQuery[Suppliers] = TableQuery[Suppliers]
 
-    // the query interface for the Coffees table
-    val coffees: TableQuery[Coffees] = TableQuery[Coffees]
+  // the query interface for the Coffees table
+  val coffees: TableQuery[Coffees] = TableQuery[Coffees]
+
+  val dropDb: DBIO[Unit] = (suppliers.schema ++ coffees.schema).drop
+
+  val createDb: DBIO[Unit] = (suppliers.schema ++ coffees.schema).create
+
+
+  val getSuppliersAboveLimit: DBIO[Seq[String]] = {
+    (for {c <- coffees if c.price < 10.0} yield c.name).result
+  }
+
+  val getCoffeeNames = coffees.map(_.name)
+
+  val getSortedLowPricedCoffeeNames = coffees.filter(x => x.price < 10.0).sortBy(_.name).map(_.name)
+
+  val plainSqlQuery = {
+    val limit = 10.0
+
+    sql"select COF_NAME from COFFEES where PRICE < $limit".as[String]
+  }
+
   try {
 
-    val f:Future[Any] = ???
+    val f = db.run(dropDb)
 
     Await.result(f, Duration.Inf)
 
   } finally db.close
 
-  val  dropDb:DBIO[Unit] = (suppliers.schema ++ coffees.schema).create
-
-  val createDb:DBIO[Unit] = (suppliers.schema ++ coffees.schema).drop
-
-  val getSuppliersAboveLimit:DBIO[Seq[String]] = (for{c <- coffees if c.price < 10.0} yield c.name ).result
 }
