30219e29e127f43f6a83dc73dca167cc00e43431
Joost de Vries <joost@ziener.org>
2015-11-23 09:09:58 +0100

    try template class

---
+++ src/main/scala/TrySlick.scala
@@ -0,0 +1,25 @@
+import MyPostgresDriver.api._
+import slick.backend.DatabasePublisher
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, Future}
+//import slick.driver.H2Driver.api._
+
+object TrySlick extends App {
+  val db = Database.forConfig("postgres")
+  try {
+
+    // The query interface for the Suppliers table
+    val suppliers: TableQuery[Suppliers] = TableQuery[Suppliers]
+
+    // the query interface for the Coffees table
+    val coffees: TableQuery[Coffees] = TableQuery[Coffees]
+
+    val f:Future[Any] = ???
+
+    Await.result(f, Duration.Inf)
+
+  } finally db.close
+  
+}
