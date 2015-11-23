d70b32ad8ca767a931f797895606a4b6a48dd060
Joost de Vries <joost@ziener.org>
2015-11-23 13:09:32 +0100

    drop if exist

--- src/main/scala/TrySlick.scala
+++ src/main/scala/TrySlick.scala
@@ -1,22 +1,54 @@
 import MyPostgresDriver.api._
+import slick.jdbc.meta.MTable
 import scala.concurrent.duration.Duration
 import scala.concurrent.{Await, Future}
+import scala.concurrent.ExecutionContext.Implicits.global
 //import slick.driver.H2Driver.api._
+
+object Actions {
+  // The query interface for the Suppliers table
+  val suppliers: TableQuery[Suppliers] = TableQuery[Suppliers]
+
+  // the query interface for the Coffees table
+  val coffees: TableQuery[Coffees] = TableQuery[Coffees]
+
+  val dropDb = {
+    val dropSuppliers = MTable.getTables(suppliers.baseTableRow.tableName).flatMap { x =>
+      if (!x.isEmpty) suppliers.schema.drop
+      else DBIO.successful(())
+    }
+    val dropCoffees = MTable.getTables(coffees.baseTableRow.tableName).flatMap { x =>
+      if (!x.isEmpty) coffees.schema.drop
+      else DBIO.successful(())
+    }
+    dropCoffees >> dropSuppliers
+  }
+
+  val createDb = (suppliers.schema ++ coffees.schema).create
+
+  val initDb = dropDb >> createDb
+
+  def withDb[A](action: DBIO[A]): A = {
+    val db = Database.forConfig("postgres")
+    try {
+
+      val f = db.run(action)
+
+      Await.result(f, Duration.Inf)
+
+    } finally db.close
+  }
+}
 
 object TrySlick extends App {
   val db = Database.forConfig("postgres")
   try {
 
-    // The query interface for the Suppliers table
-    val suppliers: TableQuery[Suppliers] = TableQuery[Suppliers]
 
-    // the query interface for the Coffees table
-    val coffees: TableQuery[Coffees] = TableQuery[Coffees]
-
-    val f:Future[Any] = ???
+    val f: Future[Any] = ???
 
     Await.result(f, Duration.Inf)
 
   } finally db.close
-  
+
 }
